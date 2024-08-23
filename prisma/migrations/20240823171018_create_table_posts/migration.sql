4f5cb36ccd12b635b2afd9bb9f8e39e152c49089
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-23 15:57:55 -0300

    feat: created the schema and the migrations in mySQL database

---
+++ prisma/migrations/20240823171018_create_table_posts/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `posts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NULL,
+    `content` VARCHAR(255) NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `posts` ADD CONSTRAINT `posts_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
